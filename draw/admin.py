e1b3f17b5268373a2e68bbc1f5a93e8dbb7711fa
edward79519 <edward79519@gmail.com>
2022-01-10 17:49:05 +0800

    add Prize, Employee, ... models

--- draw/admin.py
+++ draw/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Employee, Prize, Title, Department, AwardList
+
 
 # Register your models here.
+admin.site.register(Employee)
+admin.site.register(Prize)
+admin.site.register(Title)
+admin.site.register(Department)
+admin.site.register(AwardList)
--- draw/models.py
+++ draw/models.py
@@ -1,3 +1,94 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=20)
+    grade = models.IntegerField()
+
+    def __str__(self):
+        return "{}_{}".format(str(self.grade).zfill(2), self.name)
+
+
+class Department(models.Model):
+    sn = models.CharField(max_length=10)
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return "{}_{}".format(self.sn, self.name)
+
+
+class Employee(models.Model):
+
+    class Location(models.TextChoices):
+        TAIPEI = "台北"
+        PINGTUNG = "屏東"
+
+    class Gender(models.TextChoices):
+        MALE = "男"
+        FEMALE = "女"
+
+    sn = models.CharField(max_length=10, unique=True)
+    dept = models.ForeignKey(
+        Department,
+        on_delete=models.PROTECT,
+        related_name="employee",
+    )
+    name = models.CharField(max_length=20)
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.PROTECT,
+        related_name="employee",
+    )
+    intime = models.DateField()
+    location = models.CharField(
+        max_length=10,
+        choices=Location.choices,
+        default=Location.TAIPEI,
+    )
+    gender = models.CharField(
+        max_length=10,
+        choices=Gender.choices,
+        default=Gender.MALE,
+    )
+    is_check = models.BooleanField(default=True)
+    had_award = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "{}_{}".format(self.sn, self.name)
+
+    def job_period(self):
+        period = timezone.localtime(timezone.now()).date() - self.intime
+        return period
+
+
+class Prize(models.Model):
+    cate = models.CharField(max_length=20)
+    name = models.CharField(max_length=50)
+    cnt = models.IntegerField(default=1)
+    time_qlfy = models.IntegerField(default=0)
+    price = models.DecimalField(max_digits=8, decimal_places=0, default=0)
+
+    def __str__(self):
+        return "{}_{}".format(self.cate, self.name)
+
+
+class AwardList(models.Model):
+    emp = models.ForeignKey(
+        Employee,
+        on_delete=models.PROTECT,
+        related_name="awardlist",
+    )
+    prize = models.ForeignKey(
+        Prize,
+        on_delete=models.PROTECT,
+        related_name="awardlist",
+    )
+    addtime = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "{}_{}".format(self.prize, self.emp.name)
